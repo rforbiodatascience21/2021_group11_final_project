36fde0c847c91fb8c797eece05723a82832b000b
sofiehonore <sofiehonore@hotmail.com>
2021-05-03 07:54:01 +0000

    Created boxplots and density plots for both countries

---
+++ R/04_analysis_visualisation_countries.R
@@ -0,0 +1,78 @@
+### Visualisation of variables split on the two countries ###
+
+## pH - in latrine ##
+# Boxplot
+my_data_clean_aug %>% 
+  ggplot(aes(site, pH)) +
+  geom_boxplot() +
+  ggtitle("pH")
+
+# Density plot
+my_data_clean_aug %>% 
+  ggplot(aes(pH)) +
+  facet_wrap(vars(site)) +
+  geom_density() +
+  ggtitle("pH")
+
+## Temperature ##
+# Boxplot
+my_data_clean_aug %>% 
+  ggplot(aes(site, Temp)) +
+  geom_boxplot() +
+  ggtitle("Temperature")
+
+# Density plot
+my_data_clean_aug %>% 
+  ggplot(aes(Temp)) +
+  facet_wrap(vars(site)) +
+  geom_density() +
+  ggtitle("Temperature")
+
+
+## Prot - Information about substrate (%) ##
+# Boxplot
+my_data_clean_aug %>% 
+  ggplot(aes(site, Prot)) +
+  geom_boxplot() +
+  ggtitle("Prot")
+
+# Density plot
+my_data_clean_aug %>% 
+  ggplot(aes(Prot)) +
+  facet_wrap(vars(site)) +
+  geom_density() +
+  ggtitle("Prot")
+
+
+
+## Carbo - Information about substrate (%) ##
+# Boxplot
+my_data_clean_aug %>% 
+  ggplot(aes(site, Carbo)) +
+  geom_boxplot() +
+  ggtitle("Carbo")
+
+# Density plot
+my_data_clean_aug %>% 
+  ggplot(aes(Carbo)) +
+  facet_wrap(vars(site)) +
+  geom_density() +
+  ggtitle("Carbo")
+
+
+
+## NH4 - made by the bacteria ##
+# Boxplot
+my_data_clean_aug %>% 
+  ggplot(aes(site, NH4)) +
+  geom_boxplot() +
+  ggtitle("NH4")
+
+# Density plot
+my_data_clean_aug %>% 
+  ggplot(aes(NH4)) +
+  facet_wrap(vars(site)) +
+  geom_density() +
+  ggtitle("NH4")
+
+
