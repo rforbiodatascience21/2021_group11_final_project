1a657ea70aee30228620935d58ca82069478fecd
SofieRossen <s175411@student.dtu.dk>
2021-05-06 13:30:40 +0000

    Added full path and conversion of plot to png

--- R/04_analysis_bacteirum_analysis.R
+++ R/04_analysis_bacteirum_analysis.R
@@ -1,30 +1,19 @@
-# Distribution of samples 
-
 # Clear workspace ---------------------------------------------------------
 rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
-library("broom")  
-library("cowplot")
-library("patchwork")
-#library("correlationfunnel")
 
 # Define functions --------------------------------------------------------
-source(file = "R/99_project_functions.R")
+source(file = "/cloud/project/R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+my_data_clean_aug <- read_tsv(file = "/cloud/project/data/03_my_data_clean_aug.tsv")
 
-# Wrangle data ------------------------------------------------------------
-#site to numeric
-my_data_clean_aug <- my_data_clean_aug %>%
-  mutate(site_class = case_when(site == "Tanzania" ~ 0,
-                                site == "Vietnam" ~ 1))
+# Visualise data ----------------------------------------------------------
+bacilli_pH_barplot <- creating_bacterium_barplot(bacterium = "Bacilli", attribute = "pH")
 
-creating_bacterium_barplot("Bacilli", "pH")
-
-
-
+# Write data --------------------------------------------------------------
+ggsave("/cloud/project/results/bacilli_pH_barplot.png")
 
 
