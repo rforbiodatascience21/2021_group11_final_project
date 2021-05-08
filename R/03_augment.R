28dc12709f2131c3980880c7d45c06e9fcdb0ae8
sofiehonore <sofiehonore@hotmail.com>
2021-05-08 10:11:01 +0000

    Added more raw data and wrangling of this

--- R/03_augment.R
+++ R/03_augment.R
@@ -11,21 +11,19 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_ENV <- read_tsv(file = "data/02_my_data_clean_ENV.tsv")
-my_data_clean_SPE <- read_tsv(file = "data/02_my_data_clean_SPE.tsv")
+my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug <- my_data_clean_SPE %>%
-  pivot_longer(cols = -Taxa,
-               names_to = "Samples",
-               values_to = "OTU_Count") %>%
-  full_join(my_data_clean_ENV, by = "Samples") %>%
+my_data_clean_aug <- my_data_clean %>%
   mutate(site = case_when(str_detect(Samples, "^T") ~ "Tanzania",
-                          str_detect(Samples, "^V") ~ "Vietnam")) %>%
-  separate(Samples, into = c("Sample", "Depth"), sep="_(?=[^_]+$)", remove=F)
+                        str_detect(Samples, "^V") ~ "Vietnam"))
 
 
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data_clean_aug,
           file = "data/03_my_data_clean_aug.tsv")
+
+
+
+
