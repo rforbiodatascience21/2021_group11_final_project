725ff9ec756a2e763ad23374fc241c3ff2c8d457
maikajensen <maika.lindkvistjensen@gmail.com>
2021-05-08 08:54:31 +0000

    Fixed PCA

--- R/04_analysis_pca.R
+++ R/04_analysis_pca.R
@@ -6,9 +6,6 @@ library("tidyverse")
 library("broom")  
 library("cowplot")
 library("correlationfunnel")
-library("devtools")
-#install_github("vqv/ggbiplot")
-library("ggbiplot")
 
 # Define functions --------------------------------------------------------
 source(file = "/cloud/project/R/99_project_functions.R")
@@ -17,19 +14,44 @@ source(file = "/cloud/project/R/99_project_functions.R")
 my_data_clean_aug <- read_tsv(file = "/cloud/project/data/03_my_data_clean_aug.tsv")
 
 # Wrangle data ------------------------------------------------------------
-
-# Remove non-numeric columns and scale to unit variance
 pca_fit <- my_data_clean_aug %>% 
   select(where(is.numeric)) %>% 
-  prcomp(scale = TRUE, center = TRUE)
+  prcomp(scale = TRUE, center = TRUE) 
 
 # Visualise data ----------------------------------------------------------
 
-# Plot rotation matrix and datapoints along PC1 and 2  
-pca_rotation <- ggbiplot(pca_fit,ellipse=TRUE, groups=my_data_clean_aug$site)
+# Plot data in PC coordinates
+pca <- pca_fit %>%
+  augment(my_data_clean_aug) %>% # combine with original data to color by site
+  ggplot(aes(.fittedPC1, .fittedPC2, color = site)) + 
+  geom_point(size = 1.5) +
+  scale_color_manual(
+    values = c(Tanzania = "#D55E00", Vietnam = "#0072B2")
+  ) +
+  theme_half_open(12) + background_grid()
 
-# Variance explained by each PC 
-pca_variance <- pca_fit %>%
+# define arrow style for plotting
+arrow_style <- arrow(
+  angle = 20, ends = "first", type = "closed", length = grid::unit(8, "pt")
+)
+
+# plot rotation matrix
+rotation <- pca_fit %>%
+  tidy(matrix = "rotation") %>%
+  pivot_wider(names_from = "PC", names_prefix = "PC", values_from = "value") %>%
+  ggplot(aes(PC1, PC2)) +
+  geom_segment(xend = 0, yend = 0, arrow = arrow_style) +
+  geom_text(
+    aes(label = column),
+    hjust = 1, nudge_x = -0.02, 
+    color = "#904C2F"
+  ) +
+  xlim(-1.25, .5) + ylim(-.5, 1) +
+  coord_fixed() + # fix aspect ratio to 1:1
+  theme_minimal_grid(12)
+
+# Look at variance explained by each PC -----------------------------------
+variance <- pca_fit %>%
   tidy(matrix = "eigenvalues") %>%
   ggplot(aes(PC, percent)) +
   geom_col(fill = "#56B4E9", alpha = 0.8) +
@@ -41,6 +63,6 @@ pca_variance <- pca_fit %>%
   theme_minimal_hgrid(12)
 
 # Write data --------------------------------------------------------------
-#write_tsv(...)
-ggsave("/cloud/project/results/pca_rotation.png") 
-ggsave("/cloud/project/results/pca_variance.png")
+ggsave(file ="/cloud/project/results/pca.png", plot = pca)
+ggsave(file ="/cloud/project/results/rotation.png", plot = rotation)
+ggsave(file ="/cloud/project/results/variance.png", plot = variance)
