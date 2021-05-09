5fd89e3e58af51914ec93504bc07c28a02c0f96d
maikajensen <maika.lindkvistjensen@gmail.com>
2021-05-09 13:18:11 +0000

    Updated PCA

--- R/04_analysis_pca.R
+++ R/04_analysis_pca.R
@@ -5,7 +5,6 @@ rm(list = ls())
 library("tidyverse")
 library("broom")  
 library("cowplot")
-#library("correlationfunnel")
 
 # Define functions --------------------------------------------------------
 source(file = "/cloud/project/R/99_project_functions.R")
@@ -18,13 +17,44 @@ pca_fit <- my_data_clean_aug %>%
   select(where(is.numeric)) %>% # select only numeric columns 
   prcomp(scale = TRUE) # scale data to unit variance and perform PCA 
 
+pca <- pca_fit %>%
+  augment(my_data_clean_aug)
+
+datapc <- pca_fit %>%
+  pluck("rotation") %>%
+  as_tibble(rownames = "varnames")
+
+data_PC1 <- pca %>%
+  pluck(".fittedPC1")
+
+data_PC2 <- pca %>%
+  pluck(".fittedPC2")
+
+datapc_PC1 <- datapc %>%
+  pluck("PC1")
+
+datapc_PC2 <- datapc %>%
+  pluck("PC2")
+
+mult <- min(
+  (max(data_PC2) - min(data_PC2)/(max(datapc_PC2)-min(datapc_PC2))),
+  (max(data_PC1) - min(data_PC1)/(max(datapc_PC1)-min(datapc_PC1)))
+)
+
+datapc <- transform(datapc,
+                    v1 = 2 * mult * (get("PC1")),
+                    v2 = 2* mult * (get("PC2"))
+)
+
 # Visualise data ----------------------------------------------------------
 
-# Plot data in PC coordinates
-pca <- pca_fit %>%
-  augment(my_data_clean_aug) %>% # combine with original data to color by site
-  ggplot(aes(.fittedPC1, .fittedPC2, color = site)) + 
-  geom_point(size = 1.3) +
+# Plot in PC coordinates
+pca_plot <- ggplot(pca, aes(x=.fittedPC1, y=.fittedPC2,color = site)) + 
+  geom_point(size = 1.3) + 
+  coord_equal() + 
+  geom_text(data=datapc, aes(x=v1, y=v2, label=varnames), size = 4, vjust=-.1, hjust=1.2, color="black") + 
+  geom_segment(data=datapc, aes(x=0, y=0, xend=v1, yend=v2), 
+               arrow=arrow(length=unit(0.2,"cm")), alpha=0.75, color="black") + 
   xlab("Principal component 1") +
   ylab("Principal component 2") +
   scale_color_manual(
@@ -32,27 +62,8 @@ pca <- pca_fit %>%
     values = c(Tanzania = "#6495ed", Vietnam = "#b22222")
   ) 
 
-# Arrow style
-arrow_style <- arrow(
-  angle = 20, ends = "first", type = "closed", length = grid::unit(8, "pt")
-)
-
-# Plot rotation matrix
-rotation <- pca_fit %>%
-  tidy(matrix = "rotation") %>%
-  pivot_wider(names_from = "PC", names_prefix = "PC", values_from = "value") %>%
-  ggplot(aes(PC1, PC2)) +
-  geom_segment(xend = 0, yend = 0, arrow = arrow_style) +
-  geom_text(
-    aes(label = column),
-    hjust = 1.2, vjust = -0.5, nudge_x = 0.02, 
-    color = "#556b2f"
-  ) +
-  xlim(-.75, .5) + ylim(-.5, .5) +
-  coord_fixed() 
-
 # Variance explained by each PC
-variance <- pca_fit %>%
+pca_variance_plot <- pca_fit %>%
   tidy(matrix = "eigenvalues") %>%
   ggplot(aes(PC, percent)) +
   geom_col(fill = "#556b2f", alpha = 0.8) +
@@ -61,9 +72,9 @@ variance <- pca_fit %>%
     labels = scales::percent_format(),
     expand = expansion(mult = c(0, 0.01))
   ) +
-  ylab("Percent of variance explained")
+  ylab("Percentage of variance explained") + 
+  xlab("Principal component")
 
 # Write data --------------------------------------------------------------
-ggsave(file ="/cloud/project/results/pca.png", plot = pca)
-ggsave(file ="/cloud/project/results/rotation.png", plot = rotation)
-ggsave(file ="/cloud/project/results/variance.png", plot = variance)
+ggsave(file ="/cloud/project/results/pca_plot.png", plot = pca_plot)
+ggsave(file ="/cloud/project/results/pca_variance_plot.png", plot = pca_variance_plot)
