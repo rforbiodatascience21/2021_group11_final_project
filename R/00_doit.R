1017f6a712a2cdf570403989e5026b6140777ea3
sofiehonore <sofiehonore@hotmail.com>
2021-05-10 21:27:11 +0000

    Changed layout of doitscript

--- R/00_doit.R
+++ R/00_doit.R
@@ -1,11 +1,16 @@
 # Run all scripts ---------------------------------------------------------
 rm(list = ls())
+
 # Make directory for results
 dir.create("/cloud/project/results", showWarnings = FALSE)
+
+# Source scripts
 source(file = "/cloud/project/R/01_load.R")
 source(file = "/cloud/project/R/02_clean.R")
 source(file = "/cloud/project/R/03_augment.R")
+
 Sys.sleep(time = 3)
+
 source(file = "/cloud/project/R/04_analysis_violinplots.R")
 source(file = "/cloud/project/R/05_analysis_heatmap.R")
 source(file = "/cloud/project/R/06_analysis_pca.R")
